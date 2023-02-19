966d152066b4e498fc4378e1fbae7d49fa5f9fcc
BeesTek <omoruyiodosa4@gmail.com>
2023-02-18 20:43:09 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= '2'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
