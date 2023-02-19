9213c6932db62cf92cf29a34b8df0fa150c0a78b
BeesTek <omoruyiodosa4@gmail.com>
2023-02-18 21:37:43 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the alphabet in lowercase, except e and q.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low != q)
+	putchar (low);
+	}
+	putchar ('\n');
+	return (0);
+}
