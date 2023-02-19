6d2ccbf1d428e3ab776a16452155157dfe965718
BeesTek <omoruyiodosa4@gmail.com>
2023-02-18 20:28:02 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Prints the last digit of a randomly generated number
  *
  * Return: Always 0 (Success)
  */
@@ -13,14 +13,23 @@ int main(void)
 {
 	int n;
 
+	int x;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	x = (n % 10);
+	if (x > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	}
+	else if (x == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, x);
+	}
+	else if (x < 6 && x != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,7 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6 or 0.
+ * main - Prints the last digit of a randomly generated number.
  *
  * Return: Always 0 (Success)
  */
@@ -14,15 +13,23 @@ int main(void)
 {
 	int n;
 
+	int x;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	n = n % 10;
-	if ((n % 10) > 5)
-		printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
-	if (n == 0)
-		printf("last digit of %d is %d and is 0\n", n, n % 10);
-	if ((n % 10) < 6 && (n % 10) != 0)
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n);
+	x = (n % 10);
+	if (x > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	}
+	else if (x == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, x);
+	}
+	else if (x < 5 && x != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+	}
 	return (0);
 }
