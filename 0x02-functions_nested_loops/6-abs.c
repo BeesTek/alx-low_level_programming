edec13cc2863bf6579d22002080f6271413d0dae
BeesTek <omoruyiodosa4@gmail.com>
2023-03-15 13:47:52 -0700

    0-create_array.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to be computed
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+		return (n);
+}
